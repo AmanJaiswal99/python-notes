f66b93a90db39becbac5402f6c83fceb2c507c1e
AmanJaiswal99 <51396136+AmanJaiswal99@users.noreply.github.com>
2020-12-24 22:55:39 +0530

    final

--- numpy.py
+++ numpy.py
@@ -42,9 +42,43 @@ arr = np.linspace(start,stop,n)
 arr = np.full((m,n),k)
 
 # create an identity matrix
-
 arr = np.eye(m)
 
 # create an array with random numbers
-
 arr = np.random.random((m,n))
+
+# mathematical operations on two numpys x and y
+
+# add two numpy
+np.add(x,y)
+
+# subtract two numpy
+np.subtract(x,y)
+
+# multipy corresponding elements of two numpy 
+np.multiply(x,y)
+
+# divide two numpy
+np.divide(x,y)
+
+# square root every element of numpy x
+np.sqrt(x)
+
+# scalor product or dot product of two numpy of same size
+np.dot(x,y)
+x.dot(y)
+
+# inner product of two matrix x and y
+np.dot(x,y)
+
+# transpose a matrix x
+x.T
+
+# sum of all elements of an a numpy x
+np.sum(x)
+
+# sum of elements of an array by row
+np.sum(x,axis=0)
+
+# sum of elements of an array by column
+np.sum(x,axis=1)
